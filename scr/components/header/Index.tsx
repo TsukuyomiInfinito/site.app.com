493aad5a59e7cde877959bf1556ab6f0306fb969
TsukuyomiInfinito <grupo.da.darknet@gmx.com>
2025-05-05 01:59:46 -0300

    Create Index.tsx

---
+++ scr/components/header/Index.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="ui fixed menu">
+      <div className="ui container center">
+        <h2>Fake Shop</h2>
+      </div>
+    </header>
+  );
+};
