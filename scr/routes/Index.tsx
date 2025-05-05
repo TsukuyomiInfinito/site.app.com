4ac402c5a9e8b0f53652f282fcf9170140548d30
TsukuyomiInfinito <grupo.da.darknet@gmx.com>
2025-05-05 02:14:37 -0300

    Add files via upload

---
+++ scr/routes/Index.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from '../components/header/Index';
+import { ProductListing } from '../components/product/list/Index';
+import { ProductDetails } from '../components/product/details/Index';
+
+export const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ProductListing />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+        <Route>404 Not Found</Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
