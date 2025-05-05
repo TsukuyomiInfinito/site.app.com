9db12034ba158ce84cf2664d54fc5dd838c1865a
TsukuyomiInfinito <grupo.da.darknet@gmx.com>
2025-05-05 02:21:13 -0300

    Delete scr/store.ts

--- scr/store.ts
+++
@@ -1,5 +0,0 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import { reducers } from './reducers/index';
-import logger from 'redux-logger';
-
-export const store = createStore(reducers, {}, applyMiddleware(logger));
